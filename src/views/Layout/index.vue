c08f89f7903e9fb978d3ee0669feb8fcdeb72304
hu <348968803@qq.com>
2021-09-06 11:06:42 +0800

    登录页面完成

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,25 +2,28 @@
   <div>
     <router-view/>
 <van-tabbar route>
+  <!-- 首页 -->
   <van-tabbar-item icon="home-o" to="/home">
     <span>首页</span>
     <template #icon>
       <i class="toutiao toutiao-shouye"></i>
     </template>
   </van-tabbar-item>
+  <!-- 视频 -->
   <van-tabbar-item icon="search" to="/video">
     <span>视频</span>
     <template #icon>
       <i class="toutiao toutiao-shipin"></i>
     </template>
   </van-tabbar-item>
-  <van-tabbar-item icon="friends-o" to="qa">
+  <!-- 问答 -->
+  <van-tabbar-item icon="friends-o" to="/qa">
     <span>问答</span>
     <template #icon>
       <i class="toutiao toutiao-wenda"></i>
     </template>
   </van-tabbar-item>
-
+<!-- 我的 -->
   <van-tabbar-item icon="setting-o" to="/profile"><span>我的</span>
     <template #icon>
       <i class="toutiao toutiao-wode"></i>
